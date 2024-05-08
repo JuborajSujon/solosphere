146147198a367f76702cf85e255164a9221b0c7a
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-08 17:11:59 +0600

    Added Navbar and img resource

--- client/src/layouts/Root.jsx
+++ client/src/layouts/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Root = () => {
   return (
     <div>
       {/* nabar */}
-      <h2 className="text-2xl">Navbar</h2>
+      <Navbar />
 
       {/* content */}
       <Outlet />
