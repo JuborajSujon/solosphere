565d5d4df2f24c3927daba757cf4305e2f1ff55f
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-08 17:49:33 +0600

    Added min hight for outlet

--- client/src/layouts/Root.jsx
+++ client/src/layouts/Root.jsx
@@ -9,7 +9,9 @@ const Root = () => {
       <Navbar />
 
       {/* content */}
-      <Outlet />
+      <div className="min-h-[calc(100vh-360px)]">
+        <Outlet />
+      </div>
 
       {/* footer */}
       <Footer />
